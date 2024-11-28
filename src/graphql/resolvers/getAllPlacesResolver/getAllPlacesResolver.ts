e5681cec6f9f377d8bd8a922dea42b6c5c556bf2
mikhailyatsenko <misha.ggg@gmail.com>
2024-11-28 12:17:01 +0100

    fix: add text review resolver (field review)

--- src/graphql/resolvers/getAllPlacesResolver/getAllPlacesResolver.ts
+++ src/graphql/resolvers/getAllPlacesResolver/getAllPlacesResolver.ts
@@ -108,6 +108,7 @@ export async function getAllPlacesResolver(
         };
       }),
     );
+
     return placesWithStats;
   } catch (error) {
     console.error("Error fetching places:", error);
